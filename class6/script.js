28d2eef8fb82a83f091e9bec6ce4b134ed75f4cc
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-06-16 13:12:01 +0100

    data types and basic operator in javascript

--- class6/script.js
+++ class6/script.js
@@ -16,3 +16,68 @@ let name = 'John';
 const PI = 3.14
 
 x = 9;
+
+
+// string
+// number
+// undefined
+// null
+// boolean
+
+// object
+// array
+
+console.log(typeof('Olu'))
+console.log(typeof("Olu"))
+console.log("I'm pretty sure")
+console.log(typeof(`Olu`))
+
+console.log(6)
+console.log("6")
+
+
+let fName;
+
+console.log(true)
+console.log(false)
+
+let studentName = ['Ade', 89, null, true, undefined]
+let studentName2 = ['Ade', 'rodah', 'soji', 'yemi', "john"]
+
+let persons ={name: 'John', age: 36, gender: 'male'}
+
+console.log( 3 * 7 )
+console.log(3 + 7)
+console.log(3 - 7 )
+console.log(3 ** 7 )
+console.log(22 / 7)
+console.log(22 % 7)
+
+console.log(3>7)
+console.log(3<7)
+console.log(3==7)
+console.log(3==3)
+console.log(3=="3")
+console.log(3 === '3')
+console.log(3 >= 7)
+console.log(3 <= 7)
+console.log(3 != 4)
+console.log(3 !== '3')
+
+console.log(4 > 3  && 10 >5) //ampersand AND 
+console.log(4 > 6  && 10 >5) //ampersand AND 
+
+console.log(4 > 6  || 10 > 5) //pipe  OR 
+
+console.log(!4 > 1)
+
+
+let count = 0
+console.log(--count)
+console.log(--count)
+
+let newCount = 0
+console.log(newCount)
+console.log(++newCount)
+console.log(++newCount)
+console.log(++newCount)
